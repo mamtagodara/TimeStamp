f07b9967549b5a698e78e52d54b0b8b072655f36
Fnu Mamta <itsmamtagodara96@gmail.com>
2024-06-16 18:38:52 +0000

    Initial Commit

--- index.js
+++ index.js
@@ -18,13 +18,33 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isInvalidDate = (date) => isNaN(date.getTime());
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your API endpoint... 
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date);
+
+  if(isInvalidDate(date)){
+    date = new Date(+req.params.date);
+  }
+  if (isInvalidDate(date)) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
-
+// Handle empty date parameter
+app.get("/api", (req, res) => {
+  const currentDate = new Date(); 
+  res.json({
+    unix: currentDate.getTime(),
+    utc: currentDate.toUTCString()
+  });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
